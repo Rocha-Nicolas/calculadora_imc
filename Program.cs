1d7f18584ba8a6bc186941b3ba8c7fdec3e326b8
Rocha-Nicolas <rocha.nicolas.ezequiel@gmail.com>
2023-04-14 16:36:14 -0300

    calculadora imc

---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string resp = null;
+            // Declarar dos Listas
+            //-Bajo peso
+            List<string> nombres_bajoPeso = new List<string>();
+            List<float> imc_bajoPeso= new List<float>();
+            //-Peso saludable
+            List<string> nombres_pesoSaludable = new List<string>();
+            List<float> imc_pesoSaludable = new List<float>();
+            //-Sobrepeso
+            List<string> nombres_sobrepeso = new List<string>();
+            List<float> imc_sobrepeso = new List<float>();
+            //-obesidad
+            List<string> nombres_obesidad = new List<string>();
+            List<float> imc_obesidad = new List<float>();
+
+            //-----Programa-----//
+
+            while (resp != "no")
+            {
+                //-----Declaracion de variables del while-------
+                float peso = 0;
+                float altura = 0;
+              
+                //-------Ingreso de Datos---------
+
+                // Solicitar al usuario que ingrese el nombre del paciente 
+                Console.Write("Por favor, ingrese el nombre del paciente: ");
+                string nombre = Console.ReadLine();
+
+                // Solicitar al usuario que ingrese su peso en kilogramos
+                do {
+                    Console.Write("Por favor, ingrese su peso en kilogramos: ");
+                    resp = Console.ReadLine();
+                    if (esEntero(resp))
+                        peso = float.Parse(resp);
+                    else Console.WriteLine("Error, Peso ingresado incorrectamente");
+                } while (peso <= 0);
+
+                // Solicitar al usuario que ingrese su altura en centimetros
+                do{
+                    Console.Write("Por favor, ingrese su altura en centimetros: ");
+                    resp = Console.ReadLine();
+                    if (esEntero(resp))
+                        altura = float.Parse(resp);
+                    else Console.WriteLine("Error, altura ingresado incorrectamente");
+                } while (altura <= 0);
+
+                //------Proceso de datos ingresados 
+
+                // Convertir la altura de centímetros a metros
+                altura = altura / 100;
+
+                // Calcular el IMC
+                float imc = peso / (altura * altura);
+
+                // Evaluar el resultado del IMC y guardar en su lista correspondiente 
+                if (imc < 18.5)
+                {
+                    nombres_bajoPeso.Add(nombre);
+                    imc_bajoPeso.Add(imc);
+                }
+                else if (18.5 <= imc && imc < 25)
+                {
+                    nombres_pesoSaludable.Add(nombre);
+                    imc_pesoSaludable.Add(imc);
+                }
+                else if (imc >= 25 && imc < 30)
+                {
+                    nombres_sobrepeso.Add(nombre);
+                    imc_sobrepeso.Add(imc);
+                }
+                else
+                {
+                    nombres_obesidad.Add(nombre);
+                    imc_obesidad.Add(imc);
+                }
+
+                // Solicitar al usuario si desea ingresar otro paciente
+                while (resp != "si" && resp != "no")
+                {
+                    Console.WriteLine("Desea ingresar otro paciente?: Si/No");
+                    resp = Console.ReadLine().ToLower();
+                    if (resp != "si" && resp != "no")
+                        Console.WriteLine("Respuesta incorrecta");
+                }
+            }
+            //------Muestra resultados de los pacientes
+            Console.WriteLine("---Resultados de Pacientes: ---");
+
+            if (nombres_bajoPeso.Count != 0)
+            {
+                Console.WriteLine("Los paceitnes con bajo peso son: ");
+
+                for (int i = 0; i < nombres_bajoPeso.Count; i++)
+                {
+                    Console.WriteLine("Nombre: " + nombres_bajoPeso[i]);
+                    Console.WriteLine("IMC: " + imc_bajoPeso[i].ToString("00.00"));
+                    Console.WriteLine("---");
+                }
+                Console.WriteLine("------------------");
+            }
+
+            if (nombres_pesoSaludable.Count != 0)
+            {
+                Console.WriteLine("Los paceitnes con peso saludable son: ");
+
+                for (int i = 0; i < nombres_pesoSaludable.Count; i++)
+                {
+                    Console.WriteLine("Nombre: " + nombres_pesoSaludable[i]);
+                    Console.WriteLine("IMC: " + imc_pesoSaludable[i].ToString("00.00"));
+                    Console.WriteLine("---");
+                }
+                Console.WriteLine("------------------");
+            }
+
+            if (nombres_sobrepeso.Count != 0)
+            {
+                Console.WriteLine("Los paceitnes con sobrepeso son: ");
+
+                for (int i = 0; i < nombres_sobrepeso.Count; i++)
+                {
+                    Console.WriteLine("Nombre: " + nombres_sobrepeso[i]);
+                    Console.WriteLine("IMC: " + imc_sobrepeso[i].ToString("00.00"));
+                    Console.WriteLine("---");
+                }
+                Console.WriteLine("------------------");
+            }
+
+            if (nombres_obesidad.Count != 0)
+            {
+                Console.WriteLine("Los paceitnes con obesidad son: ");
+
+                for (int i = 0; i < nombres_obesidad.Count; i++)
+                {
+                    Console.WriteLine("Nombre: "+ nombres_obesidad[i]);
+                    Console.WriteLine("IMC: " + imc_obesidad[i].ToString("00.00"));
+                    Console.WriteLine("---");
+                }
+                Console.WriteLine("------------------");
+            }
+
+            // Esperar a que el usuario presione una tecla para salir
+            Console.ReadKey();
+        }
+
+        static bool esEntero(string valor)
+        {
+            int numero;
+            if (int.TryParse(valor, out numero))
+            {
+                if (numero > 0)
+                    return true;
+                else return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Calculadora_de_IMC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculadora_de_IMC")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("4b8f7caa-cc15-4aa0-973b-942c1f78d042")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
